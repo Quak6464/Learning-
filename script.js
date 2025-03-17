9764b986a22a8540b87f7c252d509bbde1fb73c6
Quak6464 <156640171+Quak6464@users.noreply.github.com>
2025-03-17 22:22:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const introScreen = document.getElementById("intro-screen");
-    const mainContent = document.getElementById("main-content");
+const music = document.getElementById("background-music");
 
-    // Wait until all resources (images, audio) are fully loaded
-    window.onload = function () {
-        introScreen.addEventListener("click", function () {
-            this.style.opacity = "0";
-            setTimeout(() => {
-                this.style.display = "none";
-                mainContent.style.display = "block";
-                setTimeout(() => {
-                    mainContent.style.opacity = "1";
-                }, 100);
-            }, 1000);
-        });
-    };
-});
+function toggleMusic() {
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
